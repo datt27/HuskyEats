77f39df310d1cd2b3ee039b744c09e0f0a67409f
skhan16 <khanboy411@hotmail.com>
2018-10-24 14:51:41 -0700

    pushing the app

---
+++ app/src/main/java/tcss360/huskyeatsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package tcss360.huskyeatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
